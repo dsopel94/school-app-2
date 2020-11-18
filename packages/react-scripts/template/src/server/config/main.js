e35715260b6ba0a06d37afbcc48fa0e424f5d189
Owner <owner@Owners-iMac.local>
2020-11-18 11:56:06 -0600

    migrating heroku

--- packages/react-scripts/template/src/server/config/main.js
+++ packages/react-scripts/template/src/server/config/main.js
@@ -1,3 +1,3 @@
-exports.dbUri = 'mongodb+srv://dsopel94:Password1@school-ms.t2ocx.mongodb.net/school_ms?retryWrites=true&w=majority';
+exports.dbUri = 'mongodb+srv://dsopel94:Password1@school-ms.t2ocx.mongodb.net/school-ms?retryWrites=true&w=majority';
 exports.jwt = 'super secret passphrase';
 exports.port = process.env.PORT || 3001;
